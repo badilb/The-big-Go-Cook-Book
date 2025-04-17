a26eb1816e6f74d717c5ff0707b447625ef66704
badilb <slovo.cz@gmail.com>
2025-04-17 15:18:30 +0500

    first recipe

---
+++ handling_errors.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+The ParseInt function takes in a string (and some other parameters) and
+returns a number num and an error err. You should inspect the err to
+see if the ParseInt function returns anything. If there is an error,
+you can handle it as you want. In this example, we panic, which exits
+the program.
+*/
+
+func main() {
+	str := "123456789"
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Number is: ", num)
+}
